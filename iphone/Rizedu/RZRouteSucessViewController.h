e8eb50c937b4c86017883c586672e3a37769f901
txie <txie2004@gmail.com>
2012-10-02 16:22:02 -0700

    add success view controller

---
+++ iphone/Rizedu/RZRouteSucessViewController.h
@@ -0,0 +1,13 @@
+//
+//  RZRouteSucessViewController.h
+//  Rizedu
+//
+//  Created by Tao Xie on 10/2/12.
+//  Copyright (c) 2012 Tao Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZRouteSucessViewController : UIViewController
+
+@end
---
+++ iphone/Rizedu/RZRouteSucessViewController.m
@@ -0,0 +1,38 @@
+//
+//  RZRouteSucessViewController.m
+//  Rizedu
+//
+//  Created by Tao Xie on 10/2/12.
+//  Copyright (c) 2012 Tao Xie. All rights reserved.
+//
+
+#import "RZRouteSucessViewController.h"
+
+@interface RZRouteSucessViewController ()
+
+@end
+
+@implementation RZRouteSucessViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
