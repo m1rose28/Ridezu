628a5225ef6f49de45e980758cb534b6b101cc26
Mark Rose <m1rose28@yahoo.com>
2012-12-05 08:55:56 -0800

    updated rzconfig.php

--- html/rzconfig.php
+++ html/rzconfig.php
@@ -1,9 +1,7 @@
 <?php
 
-$rzsystem='www';
-$rzversion="1204"; // this is the version for javascript and css
-$rzdomain='ridezu.com';
-$rzhost="$rzsystem.$rzdomain";
+session_start();
+$_SESSION['mode']="";
 
 //check server names and https values and re-direct if needed
 
@@ -11,12 +9,25 @@ $s=$_SERVER['SERVER_NAME'];
 $h=$_SERVER['HTTPS'];
 $u=$_SERVER['REQUEST_URI'];
 
-if($h==false or $s==$rzdomain){
-  $url = "https://" . $rzhost . $u;
+if($s=="stage.ridezu.com"){
+	$rzsystem='stage';
+	$_SESSION['mode']="dev";
+	}
+
+if($s=="www.ridezu.com"){
+	$rzsystem='www';
+	$_SESSION['mode']="prod";
+	}
+
+
+$rzversion="1204"; // this is the version for javascript and css
+$rzdomain='ridezu.com';
+$rzhost="$rzsystem.$rzdomain";
+
+if($h==false or $s!=$rzhost){
+  $url = "https://".$rzhost."/".$u;
   header("Location: $url");
   exit;
-}
-
-session_start(); 
+  }
 
 ?>
