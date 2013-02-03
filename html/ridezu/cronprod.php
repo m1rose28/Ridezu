fe7bc81ad85b9a85d373e3c97e0d5621e14b8157
Mark Rose <m1rose28@yahoo.com>
2013-02-02 17:26:55 -0800

    updated prod cron job

--- html/ridezu/cronprod.php
+++ html/ridezu/cronprod.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Cron prod job";
+echo "Peod dev job";
 
+$lines = file('https://www.ridezu.com/ridezu/api/v/1/rides/eventstate/COMPLETE');
+
+$lines1 = file('https://www.ridezu.com/ridezu/api/notification/NotificationHandler.php');
+
+$lines1 = file('https://www.ridezu.com/ridezu/api/v/1/reminder');
+
+$lines1 = file('https://www.ridezu.com/ridezu/api/v/1/balance/virtual/fixBalance');
 ?>
